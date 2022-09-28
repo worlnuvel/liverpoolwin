d991d452da507974aa7f8e378af0cbad918868b6
worlnuvel <113837024+worlnuvel@users.noreply.github.com>
2022-09-28 12:01:17 +0900

    Add files via upload

---
+++ Assets/Scripts/Square.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Square : MonoBehaviour
+{
+
+public Sprite change_img;
+Image thisImg;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        thisImg = GetComponent<Image>();
+
+        float x = Random.Range(-3.0f, 3.0f);
+        float y = Random.Range(3.0f, 5.0f);
+
+        transform.position = new Vector3(x,y,0);
+
+        float size = Random.Range(0.5f, 1.5f);
+        transform.localScale = new Vector3(size, size, 1);
+    }
+
+    void ChangeImage()
+    {
+        thisImg.sprite = change_img;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < -5.0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "balloon")
+        {
+            gameManager.I.gameOver();
+        }
+    }
+}
